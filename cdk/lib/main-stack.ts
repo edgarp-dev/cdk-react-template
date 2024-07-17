4cba2f1d2031debbee4cdadf3ad0d0140a24d218
Edgar Perez <edgar@cbqasolutions.com>
2024-07-17 16:29:12 -0600

    [NEW] Added project

---
+++ cdk/lib/main-stack.ts
@@ -0,0 +1,82 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+export class MainStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const artifactBucket = new s3.Bucket(this, 'PipelineArtifactBucket', {
+      versioned: true,
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const buildOutput = new codepipeline.Artifact();
+
+    const githubToken = ssm.StringParameter.valueFromLookup(this, '/github/token');
+
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'edgarp-dev',
+      repo: 'cdk-react-template',
+      branch: 'main',
+      oauthToken: cdk.SecretValue.unsafePlainText(githubToken),
+      output: sourceOutput,
+    });
+
+    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            'runtime-versions': {
+              nodejs: '20',
+            },
+            commands: [
+              'cd app',
+              'npm install'
+            ],
+          },
+          build: {
+            commands: [
+              'npm run build'
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'app/build',
+          files: '**/*',
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+    });
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'Build',
+      project: buildProject,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+
+    new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'MyAppPipeline',
+      artifactBucket,
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'Build',
+          actions: [buildAction],
+        },
+      ],
+    });
+  }
+}
